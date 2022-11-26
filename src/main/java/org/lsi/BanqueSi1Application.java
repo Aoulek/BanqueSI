93754665f8cd5307f4d7dd2b9d27c9ce8d075a8b
unknown <ibtissam.aoulek@etu.uae.ac.ma>
2022-11-26 23:33:48 +0100

    Add Project

---
+++ src/main/java/org/lsi/BanqueSi1Application.java
@@ -0,0 +1,13 @@
+package org.lsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueSi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueSi1Application.class, args);
+	}
+
+}
---
+++ src/main/java/org/lsi/entities/Versment.java
@@ -0,0 +1,6 @@
+package org.lsi.entities;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+@Entity
+@DiscriminatorValue("V")
+public class Versment extends Operation{}
